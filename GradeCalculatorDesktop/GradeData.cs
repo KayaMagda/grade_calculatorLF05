dc72b0a590fb598a3be0390676a5ab3c02b4272d
kaya-magda <kayakopp@hotmail.de>
2022-04-27 11:17:15 +0200

    studentClass and gradeData class with rudimentary static methods

---
+++ GradeCalculatorDesktop/GradeData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculatorDesktop
+{
+    public class GradeData
+    {
+        public int percentagePB1 { get; set; }
+        public int percentageProject { get; set; }
+        public int percentagePresentation { get; set; }
+        public int percentageVariableOne { get; set; }
+        public int percentageVariableTwo { get; set; }
+        public int percentageWiSo { get; set; }
+        public int? percentageOralAssessment { get; set; }
+
+        static int calculateGrade(int percentage)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ GradeCalculatorDesktop/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculatorDesktop
+{
+    public class Student
+    {
+        public int studentId { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public GradeData gradeData { get; set; }
+
+        static void serializeStudent(Student student)
+        {
+
+        }
+
+    }
+}
